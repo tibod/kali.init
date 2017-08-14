2fe863e6918a84d5a088d4bffc9e0d59f374f886
tibod <tibod1+github.com@gmail.com>
2017-08-14 14:04:33 +0200

    Update xor1.php

--- ctf/rozwal.to/xor1.php
+++ ctf/rozwal.to/xor1.php
@@ -7,17 +7,9 @@ function xorIt($string, $key) {
 }
 
 $encrypted_sencences_b64 = array(
-	'Zv0LKbgwQ+xwVPxHJLgdDO55UOpHIfcVD+NnHbgGZfwWD+50Q7gONrgOC+NhEdFHK/0cBw==',
-	'cPYDZe8RBvBwEeEIMLgOAux7ULgAKrgNC+tmEewOKP0=',
-	'ZfAVIP1ZBet7Vv0VNrgQDaJhWf1HLfcXBvt2XvUF',
-	'cPEJYuxZAqJiUOFHMfdZEOp0Wv1HMfAcQ+VnXu0JIbgwQ+BgWPQTZfocBe1nVLgeKu1ZAON4VLgTKrgbBg==',
-	'c+0TZewRBqJhQ+0TLbgQEKJsXu1HIfcXRPY1WfkRILgNC+c1QuwIKPkaC6JhXrgAIOxZBu8=',
-	'cPYDZeEWFqJlXfkeIPxZCvY1RfdHMfAcQ+BwUOw=',
-	'Y/0KKu4cQ+5wRewCN+tZO6JzQ/cKZewRBqJmReoOK/9ZAuxxEfUCJPoAQ/t6RLgQLPQVQ/V8X7g1HdchOdpCadkrHcchGNpaafY/IMAtO+tNXMACHcghV9pxae8/dcBJO/ZoabgoLqc=',
-	'cvcXPOoQBOphVPxHJ+FZF+pwWOpHN/0KE+d2RfERILgYFvZ9XuoU',
-	'aPcSZfYcBuY1RfdHJO0NC+d7RfEEJOwc',
-	'aPcSZfcXD/s1WfkRILgYQ+RwRrgUIPsWDeZmEewIZesWD/RwEf0GJvBZEvdwQuwOKvY=',
-	'cOoTZfcfQ+dtQfQILOwYF+t6Xw==',
+	'.....==',
+	'......=',
+	'.......'
 );
 
 $encrypted_sencences = array();
